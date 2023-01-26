5807eb343408bfb092fb67eb0f114f95e377916b
Felipe Buriticá <andres.ruizburitica@gmail.com>
2023-01-26 09:11:25 -0500

    custom admin templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from django.utils.html import format_html_join
-from django.utils.safestring import mark_safe
 
 from . import models
 
 
+class MyAdminSite(admin.AdminSite):
+    site_header = 'Blog administration panel'
+    site_header = 'Blog Admin'
+    index_title = 'Blog site administration'
+    app_index_template = 'blog/admin/blog/app_index.html'
 
 
-@admin.register(models.Comment)
+admin_site = MyAdminSite(name='my_admin')
+
+@admin.register(models.Comment, site=admin_site)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['user', 'post', 'body', 'created', 'active']
     exclude = ['email']
@@ -17,6 +22,6 @@ class CommentAdmin(admin.ModelAdmin):
     ordering = ['active', 'created']
 
 
-admin.site.register(models.Contact)
-admin.site.register(models.Post)
-admin.site.register(models.Tag)
+admin_site.register(models.Contact)
+admin_site.register(models.Post)
+admin_site.register(models.Tag)
--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog.apps.BlogConfig',
+    'blog.apps.BlogAdminConfig',
 ]
 
 MIDDLEWARE = [
